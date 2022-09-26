b95e4f4ffa8f4cee60770f20e29db13456365b54
Muhammad Alyan <alyan0332@gmail.com>
2022-09-26 22:03:57 +0500

    fixing deployment issue

--- pages/index.js
+++ pages/index.js
@@ -7,31 +7,35 @@ import spacetime from 'spacetime';
 
 export default function Home() {
   const [showMore, setShowMore] = useState(false);
-  const isEvening = spacetime.now().dayTime() === 'evening';
+  const isEvening = spacetime.now().dayTime() === 'evening' || 'good night';
 
   return (
     <>
       <Head>
         <title>Clock</title>
       </Head>
-      <section
-        className={`h-screen overflow-hidden bg-cover bg-[center_center] lg:flex lg:items-center lg:justify-between ${
+      <div
+        className={`bg-cover bg-[center_center] ${
           isEvening ? 'bg-night' : 'bg-day'
         }`}
       >
-        <div className="lg:mx-auto lg:max-w-[1100px]">
-          <div
-            className={`flex flex-col p-4 ${
-              showMore ? 'h-[50vh]' : 'h-[55vh]'
-            }`}
-          >
-            {!showMore && <RandomQuote />}
-            <Time showMore={showMore} setShowMore={setShowMore} />
-          </div>
+        <section
+          className={`h-screen overflow-hidden lg:flex lg:items-center lg:justify-between`}
+        >
+          <div className="lg:mx-auto lg:max-w-[1100px]">
+            <div
+              className={`flex flex-col p-4 ${
+                showMore ? 'h-[50vh]' : 'h-[55vh]'
+              }`}
+            >
+              {!showMore && <RandomQuote />}
+              <Time showMore={showMore} setShowMore={setShowMore} />
+            </div>
 
-          {showMore && <MoreDetails d />}
-        </div>
-      </section>
+            {showMore && <MoreDetails d />}
+          </div>
+        </section>
+      </div>
     </>
   );
 }
